639006d137525e3ac124492de3449a268d562046
Yiomar R <yiomar.rc@gmail.com>
2023-03-09 16:33:34 -0500

    feat(YiomarComponents): Add a new text

--- src/components/YiomarComponent.jsx
+++ src/components/YiomarComponent.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const YiomarComponent = () => {
-  return <div>Hola Soy Yiomar</div>;
+  return (
+    <div>
+      <br />
+      Hola Soy Yiomar
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quos quo
+        ut? Nulla harum molestias, exercitationem saepe distinctio nesciunt
+        corporis eveniet iste beatae vero hic illum expedita reprehenderit
+        tempora fuga?
+      </p>
+    </div>
+  );
 };
 
 export default YiomarComponent;
