bca2c1d89471f6491e4d6e77f1271473f91f31a8
CINTERNACIONAL\mmontes <mmontes@cinternacional.com.pe>
2023-03-09 16:31:20 -0500

    feature(MarayComponent): add new text

--- src/components/MarayComponent.jsx
+++ src/components/MarayComponent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const MarayComponent = () => {
-  return <div>Hola Soy Maray</div>;
+  return <div>¡¡¡Hola Soy Maray, probando el GitFlow!!!</div>;
 };
 
 export default MarayComponent;
